6c17c8c9f1e8831e87273db781709fe7a167d10d
Terji Djurhuus Olsen <1067391@ucn.dk>
2019-04-15 11:06:44 +0200

    Setup npm's: init for package.json, install express with no view and install nodemon.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// Handling Errors pass app.use.
+// From Academind - tdo
+app.use((req, res, next) => {
+  const error = new Error("Not found, tdo - Status 404?");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
