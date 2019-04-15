ad5176ced95b817a1074fdbba44d319de7d1e50d
Terji Djurhuus Olsen <1067391@ucn.dk>
2019-04-15 12:11:36 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    quotes: [2, "double", "avoid-escape"],
+    "linebreak-style": [2, "unix"]
+  }
 };
