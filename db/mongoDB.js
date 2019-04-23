f31972f6d84dddedfa1100a18f5fe243c4f40d64
Terji Djurhuus Olsen <1067391@ucn.dk>
2019-04-23 11:57:49 +0200

    Added to db --> MongoDB

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -6,3 +6,36 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+
+//The require(‘mongoose’) call returns a Singleton object.
+const mongoose = require("mongoose");
+
+const server = "exer6BookDb:27017"; //'jesper:1tester@ds159377.mlab.com:59377'; // REPLACE WITH YOUR DB SERVER
+const database = "testerdb"; // REPLACE WITH YOUR DB NAME;
+
+// exer6BookDb:27017
+
+class Database {
+  constructor() {}
+
+  connectWithPromise() {
+    mongoose
+      .connect(`mongodb://${server}/${database}`)
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch(err => {
+        console.error("Database connection error");
+      });
+  }
+
+  connectWithCallback() {
+    mongoose.connect(`mongodb://${server}/${database}`, err => {
+      if (err) return console.log(err);
+      console.log("Database connection successful");
+    });
+  }
+}
+
+module.exports = new Database();
