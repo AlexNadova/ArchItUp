21458eed226db4591cd80acbccc08f5bab232a61
Terji Djurhuus Olsen <1067391@ucn.dk>
2019-04-24 10:50:24 +0200

    bcrypt, jwt, userModel, userController

--- models/userModel.js
+++ models/userModel.js
@@ -4,16 +4,22 @@ const { Schema } = mongoose;
 
 const userModel = new Schema({
   //_id: mongoose.Schema.Types.ObjectId,
-  firstName: { type: String },
-  lastName: { type: String },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dateOfBirth: { type: Date },
+  country: { type: String },
+  city: { type: String },
   email: {
     type: String,
     required: true,
     unique: true,
     match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
-  phone: { type: String },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  fieldOfFocus: { type: Array },
+  education: { type: Array },
+  workExperience: { type: Array }
 });
 
 module.exports = mongoose.model("User", userModel);
