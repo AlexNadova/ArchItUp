8974f78418b29d69c845b87414f9c408a1f165fd
Alexandra Nadova <alexandranadova@gmail.com>
2019-05-23 13:23:56 +0200

    control-access-allow-origin changed to our website

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
